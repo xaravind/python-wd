969a7dfbadcc199848dd2729510d1d63e68100d3
Aravind <166210768+xaravind@users.noreply.github.com>
2025-12-01 04:18:56 +0000

    day5

--- 01.sum_and_avg.py
+++ 01.sum_and_avg.py
@@ -9,4 +9,9 @@ for i in range(10):
     total += num
 
 print("The sum of 10 numbers is : ", total)
-print("The avg of 10 numbers is : ", total/10)
+# print("The avg of 10 numbers is : ", total/10)
+# ways to print the ouput in same way with below formatters
+# print("The average of 10 numbers is: "+str(total/10))
+# print("The average of 10 numbers is: %f"%(total/10))
+# print("The average of 10 numbers is: {0}".format(total/10))
+print(f"The average of 10 numbers is: {total/10}")
