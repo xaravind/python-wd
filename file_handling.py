c5b1daad631d0dc2b68b3d640215a466dae0f5e5
Aravind <166210768+xaravind@users.noreply.github.com>
2025-12-04 06:05:54 +0000

    day8

--- file_handling.py
+++ file_handling.py
@@ -11,5 +11,12 @@ for  i in range(10):
     line = fh.readline()
     print(line, end="")
 
+# for line in fh.readlines():
+#    print(line)
 
 fh.close()
+
+
+with open("args.py") as fh:
+    for line in fh.readlines():
+        print(line)
